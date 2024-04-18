60dd51f79782776b6fa49e6d85c4ff13eb4dca83
cirobermudezmarquez <cirofabian.bermudez@gmail.com>
2024-04-18 16:49:51 +0200

    formaters script backup

---
+++ scripts/install_formaters.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo "Downloading Verible and Stylua"
+VERIBLE_FILE=verible.tar.gz
+STYLUA_FILE=stylua.zip
+
+wget https://github.com/chipsalliance/verible/releases/download/v0.0-3638-ge3ef2a37/verible-v0.0-3638-ge3ef2a37-linux-static-x86_64.tar.gz -O $SCRIPT_DIR/VERIBLE_FILE
+wget https://github.com/JohnnyMorganz/StyLua/releases/download/v0.20.0/stylua-linux-x86_64.zip -O $SCRIPT_DIR/STYLUA_FILE
+
+
+
+
+
