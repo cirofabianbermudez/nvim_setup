66e7bd2b00f1f3ff9b66025589b480f3bde1be0e
cirofabianbermudez <cirofabian.bermudez@gmail.com>
2024-04-27 14:39:40 +0200

    feat: fix missing files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"williamboman/mason.nvim",
-    lazy = false,
+		lazy = false,
 		priority = 1000,
 		config = function()
 			require("mason").setup()
@@ -11,7 +11,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "texlab", "clangd", "verible" , "ruff_lsp"},
+				ensure_installed = { "lua_ls", "texlab", "clangd", "verible", "ruff_lsp" },
 			})
 		end,
 	},
@@ -27,6 +27,20 @@ return {
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, {})
+
+			local lsp_on = false
+			function Toggle_lsp()
+				if not lsp_on then
+					print("Start Lsp.")
+					vim.cmd("LspStart")
+				else
+					print("Stop Lsp")
+					vim.cmd("LspStop")
+				end
+				lsp_on = not lsp_on
+			end
+
+			vim.api.nvim_set_keymap("n", "<leader>el", "<cmd>lua Toggle_lsp()<CR>", {})
 		end,
 	},
 }
