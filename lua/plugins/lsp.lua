b23f3cc3e2e9aa2a07114a94ddd0184b6c43b324
cirofabianbermudez <cirofabian.bermudez@gmail.com>
2024-05-20 01:18:39 +0200

    lsp disable

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,7 +11,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "texlab", "clangd", "verible", "ruff_lsp" },
+				ensure_installed = { "lua_ls", "texlab", "clangd", "verible"},
 			})
 		end,
 	},
@@ -19,11 +19,10 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.verible.setup({})
-			lspconfig.clangd.setup({})
-			lspconfig.texlab.setup({})
-			lspconfig.ruff_lsp.setup({})
+			--lspconfig.lua_ls.setup({})
+			--lspconfig.verible.setup({})
+			--lspconfig.clangd.setup({})
+			--lspconfig.texlab.setup({})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, {})
