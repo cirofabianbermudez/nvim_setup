8f3af268b2a5f1df453669338ba8769c72d98bda
cirofabianbermudez <cirofabian.bermudez@gmail.com>
2024-06-01 17:19:59 +0200

    fix: less plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,10 +3,13 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "c", "lua", "cpp", "python", "verilog", "vim", "vimdoc" },
+			ensure_installed = { "c", "lua", "cpp", "python", "vim", "vimdoc" },
 			sync_install = false,
 			auto_install = false,
-			highlight = { enable = true },
+			highlight = { 
+        enable = true ,
+        disable = { "verilog", "systemverilog" }
+      },
 		})
 	end,
 }
