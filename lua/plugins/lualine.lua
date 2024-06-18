4feace4f6d3f1242346f4831e9e66aa2f5bd9301
cirobermudezmarquez <cirofabian.bermudez@gmail.com>
2024-06-18 12:30:23 +0200

    feat: last edit to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,6 +10,17 @@ return {
 				section_separators = { left = "", right = "" },
 				disabled_filetypes = { "neo-tree" },
 			},
+      sections = {
+        lualine_x = {{
+          function()
+            local file = vim.fn.expand('%:p')
+            if file == '' then return '' end
+            local stat = vim.loop.fs_stat(file)
+            if not stat then return '' end
+            return "Last edit: " .. os.date('%Y/%m/%d %H:%M:%S', stat.mtime.sec)
+          end
+        },'encoding', 'fileformat', 'filetype'},
+      },
 		})
 	end,
 }
