2aa259ade4f61c1c91fb134322a4bb0d77149256
cirobermudezmarquez <cirofabian.bermudez@gmail.com>
2024-06-18 12:50:18 +0200

    feat: relative edit time

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -11,15 +11,44 @@ return {
 				disabled_filetypes = { "neo-tree" },
 			},
       sections = {
-        lualine_x = {{
-          function()
-            local file = vim.fn.expand('%:p')
-            if file == '' then return '' end
-            local stat = vim.loop.fs_stat(file)
-            if not stat then return '' end
-            return "Last edit: " .. os.date('%Y/%m/%d %H:%M:%S', stat.mtime.sec)
-          end
-        },'encoding', 'fileformat', 'filetype'},
+        lualine_x = {
+        --  {
+        --    function()
+        --      local file = vim.fn.expand('%:p')
+        --      if file == '' then return '' end
+        --      local stat = vim.loop.fs_stat(file)
+        --      if not stat then return '' end
+        --      return "Last edit: " .. os.date('%Y/%m/%d %H:%M:%S', stat.mtime.sec)
+        --    end
+        --  },
+          {
+            function()
+              local file = vim.fn.expand('%:p')
+              if file == '' then return '' end
+              local stat = vim.loop.fs_stat(file)
+              if not stat then return '' end
+              local last_modified = stat.mtime.sec
+              local current_time = os.time()
+              local diff = os.difftime(current_time, last_modified)
+
+              local minutes = math.floor(diff / 60)
+              local hours = math.floor(minutes / 60)
+              local days = math.floor(hours / 24)
+
+              local message = "Last-edit: "
+              local tail
+              if days > 0 then
+                tail = string.format("%dd ago", days)
+              elseif hours > 0 then
+                tail = string.format("%dh ago", hours)
+              elseif minutes > 0 then
+                tail =  string.format("%dm ago", minutes)
+              else
+                tail = "Just now"
+              end
+              return message .. tail
+            end
+          },'encoding', 'fileformat', 'filetype'},
       },
 		})
 	end,
